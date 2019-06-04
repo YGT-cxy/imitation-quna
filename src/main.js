ac3d6fa8afc27ca7b63b1f6f3a4f485cddc71c40
DESKTOP-E8FPO5M\YGT <1159514579@qq.com>
2019-06-05 07:02:45 +0800

    上传server端代码

--- src/main.js
+++ src/main.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import Vue from 'vue';
-import App from './App.vue';
-import VueAwesomeSwiper from 'vue-awesome-swiper';
-import VueCordova from 'vue-cordova';
-import store from './store/index.js';
-import router from './router.js';
-import axios from 'axios';
-// import flexible from './assets/lib/flexible.js';
-import './assets/styles/reset.css';
-import './assets/styles/border.css';
-import './assets/styles/iconfont.css';
-import 'swiper/dist/css/swiper.css';
-
-Vue.config.productionTip = false;
-
-Vue.use(VueAwesomeSwiper);
-Vue.use(VueCordova);
-
-// 配置axios
-axios.defaults.baseURL = 'http://zjyegt.cn:8888';
-Vue.prototype.$axios = axios;
-
-const app = new Vue({
-  router,
-  store,
-  render: h => h(App),
-  data() {
-    return {
-        isBack: false
-    };
-  },
-  mounted() {
-    const _this = this;
-    document.addEventListener("backbutton", function(e) {
-        e.preventDefault();
-        if(_this.isBack) {
-            navigator.app.exitApp();
-        } else {
-            _this.isBack = true;
-            setTimeout(() => {
-                _this.isBack = false;
-            }, 2000);
-        }
-    }, false);
-  }
-}).$mount('#app');
-=======
 import Vue from 'vue';
 import App from './App.vue';
 import VueAwesomeSwiper from 'vue-awesome-swiper';
@@ -94,4 +46,3 @@ new Vue({
     }, false);
   }
 }).$mount('#app');
->>>>>>> 抽离接口到api文件，增加serve端文件
